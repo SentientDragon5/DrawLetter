363d3c06b6bd6808cf1d77e07ed8bf51b10a283e
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2019-01-23 18:22:46 -0800

    Add 'b' letter

--- Pen.py
+++ Pen.py
@@ -21,7 +21,27 @@ def a(x,y,size):
             skip = True
         else:
             pen.goto(num)
+            
+def b(x,y,size):
+    skip = False
+    a = [
+        ("l"),
+        (x,y),
+        (x,y - size*4),
+        (x + size*2,y - size*4),
+        (x + size*2,y - size*2),
+        (x,y - size*2),
+        ]
+    for num in a:
+        if skip == True:
+            pen.up(); pen.goto(num); pen.down()
+        skip = False
+        if num == "l":
+            skip = True
+        else:
+            pen.goto(num)
 
 a(0,0,10)
+b(30,0,10)
         
 #(x,y),
