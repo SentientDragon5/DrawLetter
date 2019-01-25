3806cedb692b5e137fab6fc36116026185fad52e
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2019-01-24 17:24:58 -0800

    add h and i

--- Pen.py
+++ Pen.py
@@ -6,14 +6,14 @@ skip = False
 #START SPOT
 x = 0
 y = 0
-size = 2
+size = 10
 print(size)
 
-alphabet = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7}
-alphabetNum = {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g'}
+alphabetLet = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9}
+alphabetNum = {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h',9:'i'}
 placeAt = {'x':x,'y':y,'size':size}
 lettLoop = 1
-for lettnum in alphabet:
+for lettnum in alphabetLet:
     print(alphabetNum[lettLoop])
     letter = {'a':[("l"),(x,y - size*1),(x + size*2,y - size*1),(x + size*2,y - size*4),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2)]}
     letter['b'] = [("l"),(x,y),(x,y - size*4),(x + size*2,y - size*4),(x + size*2,y - size*2),(x,y - size*2),]
@@ -22,11 +22,16 @@ for lettnum in alphabet:
     letter['e'] = [("l"),(x + size*2,y - size*4),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y - size*3),(x,y - size*3)]
     letter['f'] = [("l"),(x + size,y - size*4),(x + size,y),(x + size*2,y),(x + size*2,y - size*1),("l"),(x + size*2,y - size*2),(x,y - size*2)]
     letter['g'] = [("l"),(x,y - size*4),(x + size*2,y - size*4),(x + size*2,y),(x,y),(x,y - size*2),(x + size*2,y - size*2)]
+    letter['h'] = [("l"),(x + size*2,y - size*4),(x + size*2,y - size*2),(x,y - size*2),(x,y - size*4),(x,y)]
+    letter['i'] = [("l"),(x + size*2,y - size*4),(x,y - size*4),(x + size*1,y - size*4),(x + size*1,y - size*2),(x + size*2,y - size*2),(x,y - size*2),("l"),(x + size*1,y - size*1),("d")]
+    
 
     for num in letter[lettnum]:
         if skip == True:
             pen.up(); pen.goto(num); pen.down()
         skip = False
+        if num == "d":
+            pen.dot(size/2)
         if num == "l":
             skip = True
         else:
@@ -34,3 +39,4 @@ for lettnum in alphabet:
         print(num)
     x += size *3
     lettLoop += 1
+#letter['e'] = [("l"),(x + size*2,y - size*4),
