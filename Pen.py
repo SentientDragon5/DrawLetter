a7fcbdc99b228f1916927f94c9d99412d4aca372
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2019-01-24 17:12:14 -0800

    Add f and g

--- Pen.py
+++ Pen.py
@@ -6,10 +6,11 @@ skip = False
 #START SPOT
 x = 0
 y = 0
-size = 20
+size = 2
+print(size)
 
-alphabet = {'a':1,'b':2,'c':3,'d':4,'e':5}
-alphabetNum = {1:'a',2:'b',3:'c',4:'d',5:'e'}
+alphabet = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7}
+alphabetNum = {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g'}
 placeAt = {'x':x,'y':y,'size':size}
 lettLoop = 1
 for lettnum in alphabet:
@@ -19,7 +20,8 @@ for lettnum in alphabet:
     letter['c'] = [("l"),(x + size*2,y - size*2),(x,y - size*2),(x,y - size*4),(x + size*2,y - size*4)]
     letter['d'] = [("l"),(x + size*2,y),(x + size*2,y - size*4),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2)]
     letter['e'] = [("l"),(x + size*2,y - size*4),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y - size*3),(x,y - size*3)]
-    
+    letter['f'] = [("l"),(x + size,y - size*4),(x + size,y),(x + size*2,y),(x + size*2,y - size*1),("l"),(x + size*2,y - size*2),(x,y - size*2)]
+    letter['g'] = [("l"),(x,y - size*4),(x + size*2,y - size*4),(x + size*2,y),(x,y),(x,y - size*2),(x + size*2,y - size*2)]
 
     for num in letter[lettnum]:
         if skip == True:
