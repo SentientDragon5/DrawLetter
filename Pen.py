73be6dc75a1b8fb96cf324a25e4697c0f79b73ac
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2019-01-30 17:48:05 -0800

    started bug
    added more letters

--- Pen.py
+++ Pen.py
@@ -9,8 +9,8 @@ y = 0
 size = 10
 print(size)
 
-alphabetLet = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'ENTER':10}
-alphabetNum = {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h',9:'i',10:'ENTER'}
+alphabetLet = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'ENTER':10,'j':11,'k':12}
+alphabetNum = {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h',9:'i',10:'ENTER',11:'j',12:'k'}
 placeAt = {'x':x,'y':y,'size':size}
 lettLoop = 1
 for lettnum in alphabetLet:
@@ -18,7 +18,7 @@ for lettnum in alphabetLet:
     letter = {' ':[]}
     letter['ENTER'] = [("ENTER")]
     letter['a'] = [("l"),(x,y - size*1),(x + size*2,y - size*1),(x + size*2,y - size*4),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2)]
-    letter['b'] = [("l"),(x,y),(x,y - size*4),(x + size*2,y - size*4),(x + size*2,y - size*2),(x,y - size*2),]
+    letter['b'] = [("l"),(x,y),(x,y - size*4),(x + size*2,y - size*4),(x + size*2,y - size*2),(x,y - size*2)]
     letter['c'] = [("l"),(x + size*2,y - size*2),(x,y - size*2),(x,y - size*4),(x + size*2,y - size*4)]
     letter['d'] = [("l"),(x + size*2,y),(x + size*2,y - size*4),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2)]
     letter['e'] = [("l"),(x + size*2,y - size*4),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y - size*3),(x,y - size*3)]
@@ -26,7 +26,8 @@ for lettnum in alphabetLet:
     letter['g'] = [("l"),(x,y - size*4),(x + size*2,y - size*4),(x + size*2,y - size*1),(x,y - size*1),(x,y - size*3),(x + size*2,y - size*3)]
     letter['h'] = [("l"),(x + size*2,y - size*4),(x + size*2,y - size*2),(x,y - size*2),(x,y - size*4),(x,y)]
     letter['i'] = [("l"),(x + size*2,y - size*4),(x,y - size*4),(x + size*1,y - size*4),(x + size*1,y - size*2),(x + size*2,y - size*2),(x,y - size*2),("l"),(x + size*1,y - size*1),("d")]
-    
+    letter['j'] = [("l"),(x + size*1,y - size*3),(x,y - size*3),(x,y - size*4),(x + size*2,y - size*4),(x + size*2,y - size*1),("l"),(x + size*2,y),("d")]
+    letter['k'] = [("l"),(x,y - size*4),(x,y - size*3),(x + size*2,y - size*4),(x,y - size*3),(x + size*2,y - size*2),(x,y - size*3),(x,y)]
 
     for num in letter[lettnum]:
         if skip == True:
@@ -36,7 +37,7 @@ for lettnum in alphabetLet:
             pen.dot(size/2)
         if num == "ENTER":
             y -= size *6
-        if num == "l":
+        if num == "l" or "d" or"ENTER":
             skip = True
         else:
             pen.goto(num)
