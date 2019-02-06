959521f33dc473d5ed2b8aabc5bfe979c5669b46
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2019-02-06 15:44:00 -0800

    Add letters o and y
    y, for any one like my monkey bro

--- Pen.py
+++ Pen.py
@@ -13,7 +13,7 @@ y = starty
 print('size: ',size)
 
 #alphabetLet = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'`':10,'j':11,'k':12}
-inStr = input('letters: abcdefghijkl \nstring : ')
+inStr = input('letters: abcdefghijklmno y \nstring : ')
 myStr = []
 for c in range(0,len(inStr),1):
     myStr.append(inStr[c])
@@ -40,6 +40,9 @@ for char in myStr:
     letter['l'] = [("l"),(x + size*1,y - size*4),(x + size*1,y)]
     letter['m'] = [("l"),(x,y - size*1),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y - size*4),("l"),(x + size,y - size*2),(x + size,y - size*4)]
     letter['n'] = [("l"),(x,y - size*1),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y - size*4)]
+    letter['o'] = [("l"),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y - size*4),(x,y - size*4),(x,y - size*2)]
+    
+    letter['y'] = [("l"),(x,y),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y),(x + size*2,y - size*4),(x + size,y - size*4)]
 
 
     for num in letter[char]:
