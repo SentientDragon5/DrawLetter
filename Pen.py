fed6deb05f2f78993b0c9f05cdde9dd0eb873273
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2019-02-06 15:54:11 -0800

    letters pqrst

--- Pen.py
+++ Pen.py
@@ -13,7 +13,7 @@ y = starty
 print('size: ',size)
 
 #alphabetLet = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'`':10,'j':11,'k':12}
-inStr = input('letters: abcdefghijklmno y \nstring : ')
+inStr = input('letters: abcdefghijklmnopqrst y \nstring : ')
 myStr = []
 for c in range(0,len(inStr),1):
     myStr.append(inStr[c])
@@ -41,6 +41,11 @@ for char in myStr:
     letter['m'] = [("l"),(x,y - size*1),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y - size*4),("l"),(x + size,y - size*2),(x + size,y - size*4)]
     letter['n'] = [("l"),(x,y - size*1),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y - size*4)]
     letter['o'] = [("l"),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y - size*4),(x,y - size*4),(x,y - size*2)]
+    letter['p'] = [("l"),(x,y - size*4),(x,y),(x + size*2,y),(x + size*2,y - size*2),(x,y - size*2)]
+    letter['q'] = [("l"),(x + size*2,y - size*4),(x + size*2,y),(x,y),(x,y - size*2),(x + size*2,y - size*2)]
+    letter['r'] = [("l"),(x,y - size*4),(x,y - size*2),(x,y - size*3),(x + size*2,y - size*2)]
+    letter['s'] = [("l"),(x + size*2,y - size*2),(x,y - size*2),(x + size*2,y - size*4),(x,y - size*4)]
+    letter['t'] = [("l"),(x + size,y - size*4),(x + size,y),("l"),(x + size*2,y - size*2),(x,y - size*2)]
     
     letter['y'] = [("l"),(x,y),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y),(x + size*2,y - size*4),(x + size,y - size*4)]
 
