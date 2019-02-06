ce52b4d94625b4ba00418040434e38a75af1ae01
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2019-02-06 15:37:40 -0800

    Added input Feature, m, n
    much testing, file name: Pen3.py ENTER keyword is "`" now

--- Pen.py
+++ Pen.py
@@ -12,17 +12,20 @@ y = starty
 
 print('size: ',size)
 
-#alphabetLet = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'ENTER':10,'j':11,'k':12}
-
-myStr = ['c','a','b',' ','b','a','g',' ','a','d','d','e','d']
-alphabet = ['a','b','c','d','e','f','g','h','ENTER','i','j',' ','k','l']
+#alphabetLet = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'`':10,'j':11,'k':12}
+inStr = input('letters: abcdefghijkl \nstring : ')
+myStr = []
+for c in range(0,len(inStr),1):
+    myStr.append(inStr[c])
+print (inStr , myStr)
+#myStr = ['c','a','b',' ','b','a','g',' ','a','d','d','e','d']
+alphabet = ['a','b','c','d','e','f','g','h','`','i','j',' ','k','l']
 placeAt = {'x':x,'y':y,'size':size}
-print (myStr)
 lettLoop = 1
-for char in alphabet:
+for char in myStr:
     
     letter = {' ':[]}
-    letter['ENTER'] = [("ENTER")]
+    letter['`'] = [("ENTER")]
     letter['a'] = [("l"),(x,y - size*1),(x + size*2,y - size*1),(x + size*2,y - size*4),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2)]
     letter['b'] = [("l"),(x,y),(x,y - size*4),(x + size*2,y - size*4),(x + size*2,y - size*2),(x,y - size*2)]
     letter['c'] = [("l"),(x + size*2,y - size*2),(x,y - size*2),(x,y - size*4),(x + size*2,y - size*4)]
@@ -35,6 +38,8 @@ for char in alphabet:
     letter['j'] = [("l"),(x + size*1,y - size*3),(x,y - size*3),(x,y - size*4),(x + size*2,y - size*4),(x + size*2,y - size*1),("l"),(x + size*2,y),("d")]
     letter['k'] = [("l"),(x,y - size*4),(x,y - size*3),(x + size*2,y - size*4),(x,y - size*3),(x + size*2,y - size*2),(x,y - size*3),(x,y)]
     letter['l'] = [("l"),(x + size*1,y - size*4),(x + size*1,y)]
+    letter['m'] = [("l"),(x,y - size*1),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y - size*4),("l"),(x + size,y - size*2),(x + size,y - size*4)]
+    letter['n'] = [("l"),(x,y - size*1),(x,y - size*4),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y - size*4)]
 
 
     for num in letter[char]:
