3fa09e16cb3037258533ab303f99aab783e2934e
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2019-02-06 17:54:50 -0800

    cleanup

--- Pen.py
+++ Pen.py
@@ -1,7 +1,7 @@
 import turtle
 pen = turtle.Turtle()
 pen.hideturtle()
-skip = False
+
 
 #START SPOT
 startx = 0
@@ -10,16 +10,14 @@ size = 10
 x = startx
 y = starty
 
-print('size: ',size)
-
-#alphabetLet = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'`':10,'j':11,'k':12}
 inStr = input('letters: abcdefghijklmnopqrstuvwxyz, ENTER = "`", SPACE = " "\nstring : ')
 myStr = []
 for c in range(0,len(inStr),1):
     myStr.append(inStr[c])
 print (inStr , myStr)
-#myStr = ['c','a','b',' ','b','a','g',' ','a','d','d','e','d']
-alphabet = ['a','b','c','d','e','f','g','h','`','i','j',' ','k','l']
+
+
+skip = False
 placeAt = {'x':x,'y':y,'size':size}
 lettLoop = 1
 for char in myStr:
