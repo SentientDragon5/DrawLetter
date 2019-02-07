f65751a0ae3be58bc7d926f11e31827ee14a43b9
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2019-02-06 17:50:22 -0800

    add uvwx z
    added rest of alphabet

--- Pen.py
+++ Pen.py
@@ -13,7 +13,7 @@ y = starty
 print('size: ',size)
 
 #alphabetLet = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'`':10,'j':11,'k':12}
-inStr = input('letters: abcdefghijklmnopqrst y \nstring : ')
+inStr = input('letters: abcdefghijklmnopqrstuvwxyz, ENTER = "`", SPACE = " "\nstring : ')
 myStr = []
 for c in range(0,len(inStr),1):
     myStr.append(inStr[c])
@@ -46,8 +46,12 @@ for char in myStr:
     letter['r'] = [("l"),(x,y - size*4),(x,y - size*2),(x,y - size*3),(x + size*2,y - size*2)]
     letter['s'] = [("l"),(x + size*2,y - size*2),(x,y - size*2),(x + size*2,y - size*4),(x,y - size*4)]
     letter['t'] = [("l"),(x + size,y - size*4),(x + size,y),("l"),(x + size*2,y - size*2),(x,y - size*2)]
-    
+    letter['u'] = [("l"),(x,y - size*2),(x,y - size*4),(x + size*2,y - size*4),(x + size*2,y - size*2)]
+    letter['v'] = [("l"),(x,y - size*2),(x + size,y - size*4),(x + size*2,y - size*2)]
+    letter['w'] = [("l"),(x,y - size*2),(x,y - size*4),(x + size*2,y - size*4),(x + size*2,y - size*2),("l"),(x + size,y - size*4),(x + size,y - size*2)]
+    letter['x'] = [("l"),(x + size*2,y - size*4),(x,y - size*2),("l"),(x,y - size*4),(x + size*2,y - size*2)]
     letter['y'] = [("l"),(x,y),(x,y - size*2),(x + size*2,y - size*2),(x + size*2,y),(x + size*2,y - size*4),(x + size,y - size*4)]
+    letter['z'] = [("l"),(x,y - size*2),(x + size*2,y - size*2),(x,y - size*4),(x + size*2,y - size*4)]
 
 
     for num in letter[char]:
@@ -75,3 +79,4 @@ for char in myStr:
     lettLoop += 1
 print(myStr)
 #letter['e'] = [("l"),(x + size*2,y - size*4),
+#(x + size*2,y - size*4),
